8e2406d2f75110c7fbcf15eedd31a6699f8e81fc
Paulo Scatena <paulo.scatena@gmail.com>
2020-03-22 02:40:39 +0100

    exemplo mvc

---
+++ src/frontend/YTUploader.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using YTUploader.Mvc.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace YTUploader.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IHostingEnvironment _env;
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> SaveFile()
+        {
+            if (Request.Form.Files.Count > 0)
+            {
+                var file = Request.Form.Files[0];
+                if (file != null && file.Length > 0)
+                {
+                    var webRoot = _env.WebRootPath;
+                    var filePath = System.IO.Path.Combine(webRoot, file.FileName);
+                    using(var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+            }
+            return Json(true);
+        }
+
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
